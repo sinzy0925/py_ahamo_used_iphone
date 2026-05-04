9aa51e8a42530dbdcb07f83dbcb788ee8e26fe2c
unknown <sinzy0925@gmail.com>
2026-05-04 12:19:25 +0900

    2026/05/04 12:19

--- build_site.py
+++ build_site.py
@@ -190,7 +190,7 @@ def main() -> int:
 
     inv_block = inventory_summary_html(repo_root)
 
-    title = "ahamo リユース品（在庫状況）"
+    title = "ahamo iPhone <br>リユース品の在庫状況"
 
     time_jst = now_jst.strftime("%Y-%m-%d %H:%M:%S")
 
@@ -354,7 +354,7 @@ def main() -> int:
 </head>
 <body>
   <h1>{title}</h1>
-  <p class="time-main">スクショ取得日時（日本時間・JST）: {time_jst}</p>
+  <p class="time-main">データ更新日時（日本時間・JST）: <br>{time_jst}</p>
 {buttons_block}
 {note_block}
 {inv_block}{script_handlers}
